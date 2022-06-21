19e03c323e1464a2e671dabbdf2fb081d913c74a
PintoAntoine <107704903+PintoAntoine@users.noreply.github.com>
2022-06-21 11:48:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from PIL import Image
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
@@ -11,12 +12,12 @@ import streamlit as st
 
 from os import listdir
 
+# Position of the page
 st.set_page_config(layout='wide')
-
 st.markdown("""
         <style>
                .css-18e3th9 {
-                    padding-top: 0rem;
+                    padding-top: 2rem;
                     padding-bottom: 10rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
@@ -30,42 +31,40 @@ st.markdown("""
         </style>
         """, unsafe_allow_html=True)
 
+# Importing data
 df = pd.read_parquet('data/df_video.parquet')
 
-airport = st.sidebar.selectbox(
-    "Airport",
-    df.homeAirportCode.unique().tolist()
-)
+image = Image.open('data/logo_vinci.png')
+col1, col2, col3, col4 = st.columns([5,2,4,3])
 
-jet_fuel = st.sidebar.slider(
-     'Jet Fuel Price',
-     200, 1000, 482)
+with col1:
+    st.markdown('<p style="font-family:Calibri; color:#1C5D84; font-size: 60px;">SMART DATAHUB</p>', unsafe_allow_html=True)
+with col2:
+    st.write("")
+with col3:
+    st.image(image, width =300)
+with col4:
+    st.write("")
 
-add_factor = st.sidebar.selectbox(
-    "Additional Factors",
-    ['None', 'Influencers Campaign', 'Promo Campaign', 'Product Launch']
-)
 
+# Side bar
+
+airport = st.sidebar.selectbox("Airport", df.homeAirportCode.unique().tolist())
+jet_fuel = st.sidebar.slider('Jet Fuel Price', 200, 1000, 482, format="%d $")
+add_factor = st.sidebar.radio( "Additional Factors", ['None', 'Influencers Campaign', 'Promo Campaign', 'Weather Hazard'])
 pred = st.sidebar.button('Predict')
 
+# Displaying a variable according to the chosen parameters
 var_to_display = ""
-if pred:
-    if jet_fuel > 500:
-        var_to_display = "_increaseFuel" 
-    if add_factor == "Influencers Campaign":
-        var_to_display = "_influencer"
-    pred = False
+if (pred | (add_factor == "Promo Campaign")) & (jet_fuel > 500):
+    var_to_display = "_increaseFuel"       
+if pred & (jet_fuel > 500) & (add_factor == "Promo Campaign"):
+    var_to_display = "_influencer"    
  
-st.title("SMART FORECASTER")
-
 df_id = df.query('homeAirportCode == @airport')
 
-fig1 = px.line(df_id.query('snapshotDate == "2022-01-03"'), 
-               x="movementDate", y=['pax'],
-               color_discrete_sequence=['black'])
-fig2 = px.line(df_id.query(f'snapshotDate == "2022-04-18"'),
-               x="movementDate", y=['pax_pred'+var_to_display],
-               color_discrete_sequence=['red', 'blue', 'green'])
+fig1 = px.line(df_id.query('snapshotDate == "2022-01-31"'),  x="movementDate", y=['pax'], color_discrete_sequence=['#1C5D84'])
+fig2 = px.line(df_id.query(f'snapshotDate == "2022-05-09"'), x="movementDate", y=['pax_pred'+var_to_display], color_discrete_sequence=['red', 'blue', 'green'])
 fig2.update_traces(patch={"line": {"dash": 'dot'}})
 
 fig_final = go.Figure()               
@@ -75,29 +74,28 @@ fig_final.update_layout(title=f'Airport {airport} | Pax forecast',
                         autosize=False,
                         width=1000, height=300,
                         margin=dict(l=0, r=0, b=0, t=40),
-                        showlegend=False)
+                        showlegend=False,
+                        font_color="#1C5D84",
+                        title_font_color="#1C5D84")
 
 st.plotly_chart(fig_final)
 
-fig1 = px.line(df_id.query('snapshotDate == "2022-01-03"'),
-               x="movementDate", y=['SPP'],
-               color_discrete_sequence=['black'])
-fig2 = px.line(df_id.query(f'snapshotDate == "2022-04-18"'),
-               x="movementDate", y=['SPP_pred'+var_to_display],
-               color_discrete_sequence=['red', 'blue', 'green'])
+fig1 = px.line(df_id.query('snapshotDate == "2022-01-31"'), x="movementDate", y=['SPP'], color_discrete_sequence=['#1C5D84'])
+fig2 = px.line(df_id.query(f'snapshotDate == "2022-05-09"'), x="movementDate", y=['SPP_pred'+var_to_display], color_discrete_sequence=['red', 'blue', 'green'])
 fig2.update_traces(patch={"line": {"dash": 'dot'}})
 
-fig3 = px.scatter(x=["2022-04-25"], y=[8], symbol_sequence=['triangle-up'])
-fig3.update_traces(marker=dict(size=30, color='red'))
-fig3.update_traces(hovertemplate="EJU moved from T1 to T2")
-
 fig_final = go.Figure()               
-fig_final.add_traces(data=fig1.data + fig2.data + fig3.data)
+fig_final.add_traces(data=fig1.data + fig2.data)
 fig_final.add_annotation(x="2022-04-25", y=8.1, text="!", showarrow=False, font=dict(size=20, color="white"))
-fig_final.update_layout(title=f'Airport {airport} | Pax forecast',
+fig_final.update_layout(title=f'Airport {airport} | SPP forecast',
                         autosize=False,
                         width=1000, height=300,
                         margin=dict(l=0, r=0, b=0, t=40),
-                        showlegend=False)
+                        showlegend=False,
+                        font_color="#1C5D84",
+                        title_font_color="#1C5D84")
+fig_final.update(layout_yaxis_range = [0, 10])
+
+fig_final.add_annotation(x="2022-05-01", y=8.94, text="U2 moved from T2 to T1", showarrow=True, font=dict(size=20, color="#DC152A"), bgcolor='#FACACF', arrowcolor='#DC152A')
 
 st.plotly_chart(fig_final)
